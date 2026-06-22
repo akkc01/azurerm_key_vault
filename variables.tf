ee704153524a83a871145f42644d37763dfe75d2
A K Verma <amitkkc01@gmail.com>
2026-06-22 22:44:10 +0530

    code added

--- variables.tf
+++ variables.tf
@@ -1,1 +1,10 @@
 
+variable "resource_groups" {
+    description = "A map of resource groups to create"
+    type        = map(object({
+        rg_name     = string
+        location = string
+        kv_name = string
+        
+    }))
+}
